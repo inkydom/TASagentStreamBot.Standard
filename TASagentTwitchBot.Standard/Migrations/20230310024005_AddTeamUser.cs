d35560f634786af08e3575dfdc5767a84112265e
Inky Dom <inky@inikdom.com>
2023-03-13 23:37:43 -0400

    added migration

---
+++ TASagentTwitchBot.Standard/Migrations/20230310024005_AddTeamUser.cs
@@ -0,0 +1,37 @@
+﻿ using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TASagentTwitchBot.Standard.Migrations
+{
+    public partial class AddTeamUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserTeams",
+                columns: table => new
+                {
+                    UserTeamId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TwitchUserName = table.Column<string>(type: "TEXT", nullable: false),
+                    TwitchUserId = table.Column<string>(type: "TEXT", nullable: false),
+                    TeamName = table.Column<string>(type: "TEXT", nullable: false),
+                    Points = table.Column<int>(type: "INTEGER", nullable: false),
+                    Season = table.Column<int>(type: "INTEGER", nullable: false),
+                    JoinDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTeams", x => x.UserTeamId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserTeams");
+        }
+    }
+}
